ba1f8fe5f74e7722a4a9243ff812b6a0cd249f1f
Kiran5258 <skirankumar97849@gmail.com>
2026-02-21 10:28:07 +0530

    add some more data

--- 6411839_week7_react/ex10_Officespacerentalapp/officespacerentalap/src/App.js
+++ 6411839_week7_react/ex10_Officespacerentalapp/officespacerentalap/src/App.js
@@ -13,6 +13,24 @@ const offices = [
     rent: 75000,
     address: "456 Tech Park, Bengaluru",
     image: "/image2.jpg"   // public/image2.jpg
+  },
+  {
+    name: "Office Three",
+    rent: 45000,
+    address: "789 Corporate Avenue, Mumbai",
+    image: "/image3.jpg"   // public/image3.jpg
+  },
+  {
+    name: "Office Four",
+    rent: 65000,
+    address: "321 Startup Lane, Hyderabad",
+    image: "/image4.jpg"   // public/image4.jpg
+  },
+  {
+    name: "Office Five",
+    rent: 50000,
+    address: "654 Innovation Drive, Pune",
+    image: "/image5.jpg"   // public/image5.jpg
   }
 ];
 
